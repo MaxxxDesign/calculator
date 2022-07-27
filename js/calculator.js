acc473e5dd7b846b4bc7a67ee4c4ec91c8865de0
Maxim <nodejs174rus@yandex.ru>
2022-07-27 20:19:04 +0500

    Комментарий к коммиту

---
+++ js/calculator.js
@@ -0,0 +1,18 @@
+/* calculator.js */
+export function sum(a, b) {
+      return a + b 
+  }
+
+  console.log(sum)
+  
+  export function subtract(a, b) {
+    return a - b
+  }
+  
+  export function multiply(a, b) {
+    return  a * b
+  }
+  
+  export function divide(a, b) {
+    return a / b
+  }
